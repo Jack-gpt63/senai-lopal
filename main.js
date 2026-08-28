9e935dedbe82139f501c11cf2df99bfc786b7af9
Jack-gpt63 <gustavo.villaverde10@gmail.com>
2026-08-27 23:01:03 -0300

    Adicionando uma função de saudação

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const data = () => {
+    let date = new Date()
+    return date.getHours() <= 12 ? "Bom dia" : date.getHours() <= 18 ? "Boa tarde" : "Boa noite"
+}
+console.log(`Olá, ${data()}!`)
 console.log("Hello, World!");
