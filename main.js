206d922a673ee6d482b6f1ca6aa72a71f83926f1
Jack-gpt63 <gustavo.villaverde10@gmail.com>
2026-09-02 16:44:37 -0300

    aprenzidado sobre user e dev

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ const data = () => {
     return date.getHours() <= 12 ? "Bom dia" : date.getHours() <= 18 ? "Boa tarde" : "Boa noite"
 }
 console.log(`Olá, ${data()}!`)
-console.log("Hello, World!");
+console.log("Olá você!");
+const na = 10
+const na_two = 20
